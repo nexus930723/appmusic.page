343537faa70fe4e190735ffaabfe6fbab8f422d7
nexus <nexusgood93new@gmail.com>
2025-10-02 18:47:10 +0800

    Initial Commit

---
+++ appmusic.page/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  appmusic.page
+//
+//  Created by 陳詠平 on 2025/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ appmusic.page/appmusic_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  appmusic_pageApp.swift
+//  appmusic.page
+//
+//  Created by 陳詠平 on 2025/10/2.
+//
+
+import SwiftUI
+
+@main
+struct appmusic_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
